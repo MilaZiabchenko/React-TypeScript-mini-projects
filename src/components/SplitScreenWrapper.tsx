e6b2242622b3d7febb2b85a8a7ed2c92752c15af
Mila Ziabchenko <mi.podgurska@gmail.com>
2025-05-22 15:02:53 +0300

    Update SplitScreenWrapper.tsx

--- src/components/SplitScreenWrapper.tsx
+++ src/components/SplitScreenWrapper.tsx
@@ -2,6 +2,7 @@ import type { ReactElement } from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
+  width: 100%;
   max-width: 1200px;
   margin: 5rem auto;
   display: flex;
